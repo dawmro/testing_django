0b8a97bd675870038d386f0adb5687be50ebf5d5
dawmro <dawidmrotek@protonmail.ch>
2023-06-22 14:12:34 -0700

    [ADD] display random article on home page

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,18 +1,25 @@
 """
 to render html webpages
 """
- 
 from django.http import HttpResponse
+from articles.models import Article
+import random
 
-HTML_STRING = """
-<h1>Hello!</h1>
-"""
 
 def home_view(request):
     """
     take in request, 
     return html as a response
     """
+    random_id = random.randint(1,2)
+    article_obj = Article.objects.get(id=random_id)   
+    H1_STRING = f"""
+    <h1>{article_obj.title} (id: {article_obj.id})!</h1>
+    """
+    P_STRING = f"""
+    <p>{article_obj.content}!</p>
+    """
+    HTML_STRING = H1_STRING + P_STRING
 
     return HttpResponse(HTML_STRING)
 
