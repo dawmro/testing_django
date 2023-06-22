a932eb64e0a89563995052a88f085308b3360bc6
dawmro <dawidmrotek@protonmail.ch>
2023-06-22 11:30:21 -0700

    [CREATE] Article class

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# create class Articles that inherits from django models.Model
+class Article(models.Model):
+    # title of an article
+    title = models.TextField()
+    # actual content of article
+    content = models.TextField()
