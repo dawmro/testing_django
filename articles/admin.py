20d4f4727b0e613029eb7ea9aa01f7c693d4d4c0
dawmro <dawidmrotek@protonmail.ch>
2023-06-23 06:52:29 -0700

    [ADD] display title in admin site

--- articles/admin.py
+++ articles/admin.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 
 from .models import Article
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
 
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
