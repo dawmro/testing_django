ab9bc3bceba815a95bf6785aabaffd43e405e10b
dawmro <dawidmrotek@protonmail.ch>
2023-06-23 08:29:52 -0700

    [ADD] search for articles

--- articles/views.py
+++ articles/views.py
@@ -4,6 +4,21 @@ from .models import Article
 
 # Create your views here.
 
+def article_search_view(request):
+    query_dict = request.GET
+    query = query_dict.get("q")
+
+    article_obj = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+
+    context = {
+        "object": article_obj,
+    }
+
+    return render(request, "articles/search.html", context=context)
+
+
 def article_detail_view(request, id=None):
     article_obj = None
     if id is not None:
