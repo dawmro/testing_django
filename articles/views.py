cb07d3f787b497d1f798c9187b6ecd9e1a2546f2
dawmro <dawidmrotek@protonmail.ch>
2023-06-23 08:39:56 -0700

    [ADD] check if query is int

--- articles/views.py
+++ articles/views.py
@@ -6,8 +6,13 @@ from .models import Article
 
 def article_search_view(request):
     query_dict = request.GET
-    query = query_dict.get("q")
-
+    
+    # check if query is int
+    try:
+        query = int(query_dict.get("q"))
+    except:
+        query = None
+        
     article_obj = None
     if query is not None:
         article_obj = Article.objects.get(id=query)
