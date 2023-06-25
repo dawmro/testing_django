73a35034c4d2f1ff253af22b615c0a160f9d0e03
dawmro <dawidmrotek@protonmail.ch>
2023-06-25 08:51:25 -0700

    [add] login view to views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username, password)
+
+    context = {}
+
+    return render(request, "accounts/login.html", context=context)
+
+
+def logout_view(request):
+
+    context = {}
+
+    return render(request, "accounts/logout.html", context=context)
+
+
+def register_view(request):
+
+    context = {}
+
+    return render(request, "accounts/register.html", context=context)
