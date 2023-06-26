04bf62cea046442e4bbdd52bc95ee3839acc4a9a
dawmro <dawidmrotek@protonmail.ch>
2023-06-25 21:56:23 -0700

    [ADD] ArticleForm

--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .models import Article
 from django.contrib.auth.decorators import login_required
-
+from .forms import ArticleForm
 # Create your views here.
 
 def article_search_view(request):
@@ -38,7 +38,12 @@ def article_detail_view(request, id=None):
 
 @login_required
 def article_create_view(request):
-    context = {}
+
+    form = ArticleForm()
+    context = {
+        "form": form,
+    }
+
     if request.method == "POST":
         title = request.POST.get("title")
         content = request.POST.get("content")
