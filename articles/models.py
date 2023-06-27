f0053c8aa5174876fe7f11e9bdf30a05881df0b3
dawmro <dawidmrotek@protonmail.ch>
2023-06-27 16:23:53 -0700

    [FIX] change TextField to CharField for title

--- articles/models.py
+++ articles/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 # create class Articles that inherits from django models.Model
 class Article(models.Model):
+    # https://docs.djangoproject.com/en/4.2/ref/models/fields/#django.db.models
     # title of an article
-    title = models.TextField()
+    title = models.CharField(max_length=100)
     # actual content of article
     content = models.TextField()
