70d3e984ca719a457c51db4af0f8e1ef83898093
dawmro <dawidmrotek@protonmail.ch>
2023-06-27 14:36:55 -0700

    [TEST] secret key strength

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-3q8@qbul6+^xlqaw%c&8wdp*bx+)n_2m6kr*hs93iu@rt_e_$3')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # check if debug is set to 1 -> True, everythig else gives False
---
+++ trydjango/tests.py
@@ -0,0 +1,14 @@
+import os
+from django.test import TestCase
+from django.contrib.auth.password_validation import validate_password
+
+class TryDjangoConfigTest(TestCase):
+    
+    def test_secret_key_strength(self):
+        SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+        try:
+            is_strong = validate_password(SECRET_KEY)
+        except Exception as e:
+            msg = f"Weak secret key {e}"
+            self.fail(msg)
+
