e3d5973937882831682690b76406b79220813b25
dawmro <dawidmrotek@protonmail.ch>
2023-06-28 18:15:56 -0700

    [TEST] check if there are any article objects in database

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from .models import Article
+
+class ArticleTestCase(TestCase):
+
+    def setUp(self):
+        # create article for test database
+        Article.objects.create(title='Hello world', content='Content of article.')
+
+    # check if there are any article objects in database
+    def test_queryset_exists(self):
+        qs = Article.objects.all()
+        self.assertTrue(qs.exists())
