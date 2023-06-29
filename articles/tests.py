a8eda648bc9fd6590a164053eb1921ea73a6f0e1
dawmro <dawidmrotek@protonmail.ch>
2023-06-28 18:35:26 -0700

    [TEST] check if slugification works correctly

--- articles/tests.py
+++ articles/tests.py
@@ -3,14 +3,32 @@ from django.test import TestCase
 # Create your tests here.
 
 from .models import Article
+from django.utils.text import slugify
+
 
 class ArticleTestCase(TestCase):
 
     def setUp(self):
-        # create article for test database
-        Article.objects.create(title='Hello world', content='Content of article.')
+        # create articles for test database
+        self.number_of_articles = 5
+        for i in range(0, self.number_of_articles):
+            Article.objects.create(title='Hello world', content=f"Content of article {i}.")
 
     # check if there are any article objects in database
     def test_queryset_exists(self):
         qs = Article.objects.all()
         self.assertTrue(qs.exists())
+
+    # check if there are is correct number article objects in database
+    def test_queryset_count(self):
+        qs = Article.objects.all()
+        self.assertEquals(qs.count(), self.number_of_articles)
+
+    # check if slugification works correctly
+    def test_hello_world_slug(self):
+        # get first article because it's slug hasn't been sufixed with anything
+        obj = Article.objects.all().order_by("id").first()
+        slug = obj.slug
+        title = obj.title
+        slugified_title = slugify(title)
+        self.assertEqual(slug, slugified_title)
