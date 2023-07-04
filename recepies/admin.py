2c0f0d8e27c5c2d955f4f130eda7b68d5ea276a6
dawmro <dawidmrotek@protonmail.ch>
2023-07-03 17:05:26 -0700

    [ADD] Admin Inlines for Foreign Keys

--- recepies/admin.py
+++ recepies/admin.py
@@ -1,7 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.contrib import admin
 from .models import RecipeIngridient, Recipe
 
 # Register your models here.
 
-admin.site.register(RecipeIngridient)
-admin.site.register(Recipe)
+User = get_user_model()  
+
+class RecipeIngridientInline(admin.StackedInline):
+    model = RecipeIngridient
+    #fields = ['name', 'quantity', 'unit', 'directions']
+    extra = 0
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngridientInline]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+admin.site.register(Recipe, RecipeAdmin)
