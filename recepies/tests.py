d9b5e56a3ae0c0d3e6c1ced916601165d3e9b974
dawmro <dawidmrotek@protonmail.ch>
2023-07-04 13:01:33 -0700

    [TEST] user password

--- recepies/tests.py
+++ recepies/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
+from .models import RecipeIngridient, Recipe
 # Create your tests here.
+
+User = get_user_model()
+
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        self.user_a = User.objects.create_user('adam', password='pass1234')
+
+    def test_user_pw(self):
+        checked = self.user_a.check_password('pass1234')
+        self.assertTrue(checked)
+
+
