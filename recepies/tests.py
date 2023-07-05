adad29d0be410571929f5a189ab00f5d236d044d
dawmro <dawidmrotek@protonmail.ch>
2023-07-05 11:33:23 -0700

    [TEST] unit measure validation

--- recepies/tests.py
+++ recepies/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
+from django.core.exceptions import ValidationError
 from .models import RecipeIngridient, Recipe
+
 # Create your tests here.
 
 User = get_user_model()
@@ -96,3 +97,27 @@ class RecipeTestCase(TestCase):
         ids = user.recipe_set.all().values_list("id", flat=True)
         qs = RecipeIngridient.objects.filter(recipe_id__in=ids)
         self.assertEqual(qs.count(), 5)
+
+    def test_unit_measure_validation_pass(self):
+        valid_unit = 'ounce'
+        ingridient = RecipeIngridient(
+            name="new",
+            quantity=10,
+            recipe=self.recipe_a,
+            unit=valid_unit
+        )
+        # verify if all fields are valid
+        ingridient.full_clean()
+
+    def test_unit_measure_validation_error(self):
+        invalid_unit = ['yellow', 'chungus', 'bread', 'arefsfsdfg']
+        for inv_unit in invalid_unit:
+            with self.assertRaises(ValidationError):
+                ingridient = RecipeIngridient(
+                    name="new",
+                    quantity=10,
+                    recipe=self.recipe_a,
+                    unit=inv_unit
+                )
+                # verify if all fields are valid
+                ingridient.full_clean()
