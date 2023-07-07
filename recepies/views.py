746d5b21a8e2804ad2f9bff198164571436e21c2
dawmro <dawidmrotek@protonmail.ch>
2023-07-06 21:22:20 -0700

    [ADD] 2 forms in 1 view

--- recepies/views.py
+++ recepies/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Recipe
 from django.contrib.auth.decorators import login_required
-from .forms import RecipeForm
+from .forms import RecipeForm, RecipeIngredientForm
 # Create your views here.
 
 # CRUD 
@@ -43,11 +43,19 @@ def recipe_create_view(request):
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id, user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
+    form2 = RecipeIngredientForm(request.POST or None)
     context = {
         "form": form,
+        "form2": form2,
         "object": obj
     }
-    if form.is_valid():
-        form.save()
+    if all([form.is_valid(), form2.is_valid()]):
+        parent = form.save(commit=False)
+        parent.save()
+        child = form2.save(commit=False)
+        child.recipe = parent
+        child.save()
+        print("form:", form.cleaned_data)
+        print("form2:", form2.cleaned_data)
         context["message"] = "Data saved."
     return render(request, "recipes/create-update.html", context=context)
