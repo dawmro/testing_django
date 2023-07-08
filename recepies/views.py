5f6003adac29a184291e4f671d3fc3f0612dfbe8
dawmro <dawidmrotek@protonmail.ch>
2023-07-08 10:26:21 -0700

    [FIX] modelformset factory for multiple forms in 1 view

--- recepies/views.py
+++ recepies/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import redirect, render, get_object_or_404
-from .models import Recipe
 from django.contrib.auth.decorators import login_required
+from django.forms.models import modelformset_factory # model form for querysets
+from django.shortcuts import redirect, render, get_object_or_404
 from .forms import RecipeForm, RecipeIngredientForm
+from .models import Recipe, RecipeIngredient
 # Create your views here.
 
 # CRUD 
@@ -43,19 +44,26 @@ def recipe_create_view(request):
 def recipe_update_view(request, id=None):
     obj = get_object_or_404(Recipe, id=id, user=request.user)
     form = RecipeForm(request.POST or None, instance=obj)
-    form2 = RecipeIngredientForm(request.POST or None)
+    # class that will be used to create formset instance
+    # Formset = modelformset_factory(Model, form=ModelForm, extra=0)
+    RecipeIngredientFormset = modelformset_factory(RecipeIngredient, form=RecipeIngredientForm, extra=0)
+    qs = obj.recipeingredient_set.all() # it has values or is empty []
+    # initialize formset with request and querty set 
+    formset = RecipeIngredientFormset(request.POST or None, queryset=qs)
+
     context = {
         "form": form,
-        "form2": form2,
+        "formset": formset,
         "object": obj
     }
-    if all([form.is_valid(), form2.is_valid()]):
+    if all([form.is_valid(), formset.is_valid()]):
         parent = form.save(commit=False)
         parent.save()
-        child = form2.save(commit=False)
-        child.recipe = parent
-        child.save()
-        print("form:", form.cleaned_data)
-        print("form2:", form2.cleaned_data)
+        for form in formset:
+            child = form.save(commit=False)
+            if child.recipe is None:
+                print("Added new")
+                child.recipe = parent
+            child.save()
         context["message"] = "Data saved."
     return render(request, "recipes/create-update.html", context=context)
