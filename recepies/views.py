dcf622454996e368dc7171b6d53b4c96b92ffb80
dawmro <dawidmrotek@protonmail.ch>
2023-07-12 12:28:30 -0700

    [ADD] ingredient model object delete

--- recepies/views.py
+++ recepies/views.py
@@ -45,6 +45,34 @@ def recipe_detail_view(request, id=None):
 
 
 @login_required
+def recipe_delete_view(request, id=None):
+    obj = get_object_or_404(Recipe, id=id, user=request.user)
+
+    if request.method == "POST":
+        obj.delete()
+        success_url = reverse("recipes:list")
+        return redirect(success_url)
+    context = {
+        "object": obj
+    }
+    return render(request, "recipes/delete.html", context=context)   
+
+
+@login_required
+def recipe_ingredient_delete_view(request, parent_id=None, id=None):
+    obj = get_object_or_404(RecipeIngredient, recipe__id=parent_id, id=id, recipe__user=request.user)
+
+    if request.method == "POST":
+        obj.delete()
+        success_url = reverse("recipes:detail", kwargs={"id": parent_id})
+        return redirect(success_url)
+    context = {
+        "object": obj
+    }
+    return render(request, "recipes/delete.html", context=context)   
+
+
+@login_required
 def recipe_create_view(request):
     form = RecipeForm(request.POST or None)
     context = {
