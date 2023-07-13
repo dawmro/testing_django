0d4979e520ccbd3ec6df744afdbe9caeec78c84e
dawmro <dawidmrotek@protonmail.ch>
2023-07-13 13:04:06 -0700

    [ADD] Static Files in Development

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles-cdn"
+STATIC_ROOT = BASE_DIR / "staticfiles-cdn" # in production we want content delivery network
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
