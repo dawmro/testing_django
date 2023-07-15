d82b1d844a25dbbaed880908f86994c44154d26d
dawmro <dawidmrotek@protonmail.ch>
2023-07-15 14:03:31 -0700

    [ADD] view for handling image uploads

--- recepies/views.py
+++ recepies/views.py
@@ -3,7 +3,7 @@ from django.forms.models import modelformset_factory # model form for querysets
 from django.urls import reverse
 from django.http import HttpResponse, Http404
 from django.shortcuts import redirect, render, get_object_or_404
-from .forms import RecipeForm, RecipeIngredientForm
+from .forms import RecipeForm, RecipeIngredientForm, RecipeIngredientImageForm
 from .models import Recipe, RecipeIngredient
 # Create your views here.
 
@@ -179,3 +179,21 @@ def recipe_ingredient_update_hx_view(request, parent_id=None, id=None):
         return render(request, "recipes/partials/ingredient-inline.html", context=context)
 
     return render(request, "recipes/partials/ingredient-form.html", context=context)
+
+
+def recipe_ingredient_image_upload_view(request, parent_id=None):
+    try:
+        parent_obj = Recipe.objects.get(id=parent_id, user=request.user)
+    except:
+        parent_obj = None
+    if parent_obj is None:
+        raise Http404
+    form = RecipeIngredientImageForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.recipe = parent_obj
+        obj.save()
+    context = {
+        "form": form
+    }
+    return render(request, "image-form.html", context=context)
