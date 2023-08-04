4fdc76715abb3d9bb7ce21b568c98585cafcb678
dawmro <dawidmrotek@protonmail.ch>
2023-08-04 11:01:29 -0700

    [ADD] call POST method by clicking meal toggle button

--- meals/views.py
+++ meals/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+from .models import Meal
+
 # Create your views here.
 def meal_queue_toggle_view(request, recipe_id=None):
+    user = request.user
+    user_id = None
+    if user.is_authenticated:
+        user_id = user.id
+    if user_id is None:
+        return
+    is_pending = Meal.objects.by_user_id(user_id).in_queue(recipe_id)
+    toggle_label = "Add to meals" if not is_pending else "Remove from meals"
     context = {
-        'recipe_id': recipe_id
+        'recipe_id': recipe_id,
+        'toggle_label': toggle_label,
+        'is_pending': is_pending
     }
     return render(request, 'meals/partials/queue-toggle.html', context)
