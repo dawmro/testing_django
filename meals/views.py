2b399e0a823e57f1a5fd1d93a55442dabe5be414
dawmro <dawidmrotek@protonmail.ch>
2023-08-04 08:49:18 -0700

    [ADD] show recipe_id using meal-toggle

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def meal_queue_toggle_view(request, recipe_id=None):
+    context = {
+        'recipe_id': recipe_id
+    }
+    return render(request, 'meals/partials/queue-toggle.html', context)
